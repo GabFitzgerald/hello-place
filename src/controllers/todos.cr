513b3ecf91137e7bb26f9efcdd3b6f1bf2802088
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-20 09:33:35 +1000

    reaf(todos controller): deleted a lot of commented code

--- src/controllers/todos.cr
+++ src/controllers/todos.cr
@@ -3,62 +3,7 @@ require "../models/todos.cr"
 class Todos < Application
   base "/todos"
 
-  ################################################
-  # CORS setup
-  ################################################
-  # def todo_url(id)
-  #   "#{TODOS_URI}/#{id}"
-  # end
-
-  # def repr(todo, base_url)
-  #   {uid:       todo._id,
-  #     title:     todo.title,
-  #     order:     todo.order,
-  #     completed: todo.completed,
-  #     url:       base_url + todo_url(todo._id),
-  #   }
-  # end
-
-  # def before_all "/todos" do |env|
-  #   # Support CORS and set responses to JSON as default.
-  #   headers env, {
-  #     "Access-Control-Allow-Origin"  => "*",
-  #     "Content-Type"                 => "application/json",
-  #     "Access-Control-Allow-Headers" => "Content-Type",
-  #   }
-  # end
-
-  # before_action :before_all 
-
-  
-
-  # ,HEAD,POST,DELETE,OPTIONS,PUT,PATCH
-
-  # options "/:id", :option_task_id do
-  #   response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH"
-  # end
-
-  # before_action :setup_cors
-
-  # def setup_cors
-  #     "Access-Control-Allow-Origin"  => "*",
-  #     "Content-Type"                 => "application/json",
-  #     "Access-Control-Allow-Headers" => "Content-Type"
-  # end
-
-  # before_all "/todos/:id" do |env|
-  #   # Support CORS and set responses to JSON as default.
-  #   headers env, {
-  #     "Access-Control-Allow-Origin"  => "*",
-  #     "Content-Type"                 => "application/json",
-  #     "Access-Control-Allow-Headers" => "Content-Type",
-  #   }
-  # end
-
-  ###########################################################
-
   getter todo : Todo { find_todo }
-
   # setter
   # property
 
@@ -68,26 +13,26 @@ class Todos < Application
     # respond_with do
     #   html template("todos.ecr")
     # end
-    # response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH"
   end
 
   # GET /todos/new
-
 
   # POST /todos
   def create
     new_todo = Todo.new(JSON.parse(request.body.as(IO))).save!
     new_todo.url = "http://localhost:3000/todos/#{new_todo.id}"
     new_todo.save!
-    render text: new_todo.to_json  
-    # render new_todo
-    # render json: { todo }
-    # render :created, text: todo.to_json
+    render text: new_todo.to_json
   end
 
   # GET /todos/:id
   def show
     render json: {todo}
+  end
+
+  get "todo/:id" do
+  
+  
   end
 
   # PATCH /todos/:id
@@ -106,12 +51,13 @@ class Todos < Application
     render text: todo.to_json 
   end
 
+  delete "/" do 
+    Todo.query.select.each { |todo| todo.delete }
+    render text: ({} of String => String).to_json
+  end
+
   options "/", :option_todo do 
-    response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH"
-    # response.["Access-Control-Allow-Origin"]  = "*"
-    # response.["Content-Type"]                = "application/json"
-    # response.["Access-Control-Allow-Headers"] = "Content-Type"
-  
+    response.headers["Access-Control-Allow-Methods"] = "GET,HEAD,POST,DELETE,OPTIONS,PUT,PATCH"  
   end
 
   options "/:id", :option_todo_id do
