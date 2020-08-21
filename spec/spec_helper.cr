f84e49c0f4fac9495f5d4199d414d735cab1a106
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 13:12:16 +1000

    okay that didn't work, this might

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -6,23 +6,20 @@ require "spec"
 
 # Helper methods for testing controllers (curl, with_server, context)
 require "../lib/action-controller/spec/curl_context"
-require "clear"
-require "action-controller"
-require "action-controller/server"
-require "../src/db/migrations/*"
-require "../src/constants.cr"
+
 require "../src/models/*"
 require "../src/controllers/*"
-# require "../src/*"
 
 # Dependent libs
+require "clear"
+# require "simple_retry"
 
-# # require "simple_retry"
+require "action-controller"
+require "action-controller/server"
 
-
-
-# # Set up testing env
-
+# Set up testing env
+require "../src/db/migrations/*"
+require "../src/constants.cr"
 
 
 Clear::SQL.init(App::PG_DATABASE_URL,
