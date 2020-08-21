2a1f5c5114aba29c5f4e3e938de4a9103e5d53ad
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 14:59:17 +1000

    no testing env

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,8 +2,8 @@ require "spec"
 
 # Your application config
 # If you have a testing environment, replace this with a test config file
-# require "../src/config.cr"
-require "./test_config.cr"
+require "../src/config.cr"
+# require "./test_config.cr"
 
 
 # Helper methods for testing controllers (curl, with_server, context)
