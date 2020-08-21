e51227b91f5571f347555f28f828c418fcb2e354
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 13:56:59 +1000

    test config??

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,7 +2,8 @@ require "spec"
 
 # Your application config
 # If you have a testing environment, replace this with a test config file
-require "./test_config"
+require "./test_config.cr"
+# require "../src/config.cr"
 
 # Helper methods for testing controllers (curl, with_server, context)
 require "../lib/action-controller/spec/curl_context"
