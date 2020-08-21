6ff5c42df2f0da9c18600dc1e0187c2ed26ef500
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 13:23:08 +1000

    simple retry added

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -12,23 +12,23 @@ require "../src/controllers/*"
 
 # Dependent libs
 require "clear"
-# require "simple_retry"
+require "simple_retry"
 
-require "action-controller"
-require "action-controller/server"
+# require "action-controller"
+# require "action-controller/server"
 
 # Set up testing env
 require "../src/db/migrations/*"
 require "../src/constants.cr"
 
 
-Clear::SQL.init(App::PG_DATABASE_URL,
-    connection_pool_size: 5)
 
-# SimpleRetry.try_to(max_attempts: 10, retry_on: DB::ConnectionRefused) do
-#   # Connect to PG
 
-# end
+SimpleRetry.try_to(max_attempts: 10, retry_on: DB::ConnectionRefused) do
+  # Connect to PG
+    Clear::SQL.init(App::PG_DATABASE_URL,
+        connection_pool_size: 5)
+end
 
 # Run the Migration
 Clear::Migration::Manager.instance.apply_all
