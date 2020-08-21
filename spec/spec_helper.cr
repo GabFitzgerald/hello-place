316705da4e5403d90e9787714ca626c230009964
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 13:05:32 +1000

    okay that didn't work, this might

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -7,26 +7,26 @@ require "spec"
 # Helper methods for testing controllers (curl, with_server, context)
 require "../lib/action-controller/spec/curl_context"
 
-# require "action-controller"
-# require "action-controller/server"
+require "action-controller"
+require "action-controller/server"
 
-# require "../src/models/*"
-# require "../src/controllers/*"
-require "../src/*"
+require "../src/models/*"
+require "../src/controllers/*"
+# require "../src/*"
 
 # Dependent libs
-# require "clear"
+
 # # require "simple_retry"
 
 
 
 # # Set up testing env
-# require "../src/db/migrations/*"
-# require "../src/constants.cr"
+require "../src/db/migrations/*"
+require "../src/constants.cr"
 
-
-# Clear::SQL.init(App::PG_DATABASE_URL,
-#     connection_pool_size: 5)
+require "clear"
+Clear::SQL.init(App::PG_DATABASE_URL,
+    connection_pool_size: 5)
 
 # SimpleRetry.try_to(max_attempts: 10, retry_on: DB::ConnectionRefused) do
 #   # Connect to PG
@@ -34,4 +34,4 @@ require "../src/*"
 # end
 
 # Run the Migration
-# Clear::Migration::Manager.instance.apply_all
+Clear::Migration::Manager.instance.apply_all
