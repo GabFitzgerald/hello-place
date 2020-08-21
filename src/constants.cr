c7913e1966467cd0337d6807b7f3bbf043024f04
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 16:52:15 +1000

    constants update

--- src/constants.cr
+++ src/constants.cr
@@ -19,7 +19,8 @@ module App
   COOKIE_SESSION_KEY    = ENV["COOKIE_SESSION_KEY"]? || "_hello_place_"
   COOKIE_SESSION_SECRET = ENV["COOKIE_SESSION_SECRET"]? || "4f74c0b358d5bab4000dd3c75465dc2c"
 
-  PG_DATABASE_URL = ENV["PG_DATABASE_URL"]? || "postgres://gab@localhost/gab"
+  # PG_DATABASE_URL = ENV["PG_DATABASE_URL"]? || "postgres://gab@localhost/gab"
+  PG_DATABASE_URL = "postgresql-globular-66920"
 
   def self.running_in_production?
     ENVIRONMENT == "production"
