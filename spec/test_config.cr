50928d9e6ba54adfd886ce4da53dee15b9b36bde
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 15:16:03 +1000

    removed env var

--- spec/test_config.cr
+++ spec/test_config.cr
@@ -1,18 +1,8 @@
-# Dependent libs
-require "clear"
-require "simple_retry"
-
-require "action-controller"
-require "action-controller/server"
-
-require "../src/db/migrations/*"
+require "../src/db/migrations/1_create_table.cr"
 require "../src/constants.cr"
 
-
-
-SimpleRetry.try_to(max_attempts: 10, retry_on: DB::ConnectionRefused) do
-    Clear::SQL.init(App::PG_DATABASE_URL,
+require "clear"
+Clear::SQL.init("postgres://gab@localhost/gab",
     connection_pool_size: 5)
-end
 
 Clear::Migration::Manager.instance.apply_all
