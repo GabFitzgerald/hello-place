0cfe73ca23362903c60184b74eb939cea1c786c7
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-21 14:03:10 +1000

    test config??

--- spec/test_config.cr
+++ spec/test_config.cr
@@ -8,9 +8,11 @@ require "action-controller/server"
 require "../src/db/migrations/*"
 require "../src/constants.cr"
 
-SimpleRetry.try_to(max_attempts: 10, retry_on: DB::ConnectionRefused) do
-    Clear::SQL.init(App::PG_DATABASE_URL,
+Clear::SQL.init(App::PG_DATABASE_URL,
         connection_pool_size: 5)
-end
+
+# SimpleRetry.try_to(max_attempts: 10, retry_on: DB::ConnectionRefused) do
+    
+# end
 
 Clear::Migration::Manager.instance.apply_all
