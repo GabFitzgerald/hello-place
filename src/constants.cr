4ecfe4638be45a5e146cac7706ea9a334d2c2e35
Gab Fitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-10-14 16:24:22 +1000

    testing github connection

--- src/constants.cr
+++ src/constants.cr
@@ -20,7 +20,8 @@ module App
   COOKIE_SESSION_SECRET = ENV["COOKIE_SESSION_SECRET"]? || "4f74c0b358d5bab4000dd3c75465dc2c"
 
   PG_DATABASE_URL = ENV["PG_DATABASE_URL"]? || "postgres://gab@localhost/gab"
-  # PG_DATABASE_URL = ENV["PG_DATABASE_URL"]? || "postgresql-globular-66920"
+  # PG_DATABASE_URL = ENV["PG_DATABASE_URL"]? || "postgres://postgresql-globular-66920"
+  DATABASE_URL = "postgres://postgresql-globular-66920"
 
   def self.running_in_production?
     ENVIRONMENT == "production"
